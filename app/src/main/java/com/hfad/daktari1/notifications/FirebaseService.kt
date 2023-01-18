3779b602fe2e751f56303c4635400121bb5877a9
AMG <abelgakuya@gmail.com>
2023-01-18 13:10:45 +0300

    Edits

--- app/src/main/java/com/hfad/daktari1/notifications/FirebaseService.kt
+++ app/src/main/java/com/hfad/daktari1/notifications/FirebaseService.kt
@@ -41,7 +41,7 @@ class FirebaseService : FirebaseMessagingService() {
         }
 
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, FLAG_ONE_SHOT)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle(message.data["title"])
             .setContentText(message.data["message"])
